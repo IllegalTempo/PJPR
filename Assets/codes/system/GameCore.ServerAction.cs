0078c9629b9c901baf669dc65f54b8615aebda4e
AfterTime <ywhangus@gmail.com>
2026-03-08 23:15:05 +0800

    just onSelectPlanet() with some todos

--- Assets/codes/system/GameCore.ServerAction.cs
+++ Assets/codes/system/GameCore.ServerAction.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Cysharp.Threading.Tasks;
 
 public partial class GameCore
 {
@@ -8,6 +9,32 @@ public partial class GameCore
     {
         int index = Random.Range(0, getMissionWithLevel(CurrentMissionLevel).Length);
         StartMission(CurrentMissionLevel, index);
-
     }
+
+    public async void onSelectPlanet(string selectedPlanetId)
+    {
+        Debug.Log($"Planet {selectedPlanetId} selected by the party leader!");
+        
+        // TODO: Trigger spaceship (or connector) movement/warp drive sequence towards the selected planet
+        
+        // minigames if they are related to the selected planet ? or unrelated cuz random
+        // int minigamesToPlay = Random.Range(3, 5); 
+        // await ProcessJourneyMinigames(minigamesToPlay);
+        
+        Debug.Log($"Arrived at planet {selectedPlanetId}!");
+        // TODO: something to be done on the planet or other minigames etc.
+    }
+
+    // private async UniTask ProcessJourneyMinigames(int minigameCount)
+    // {
+    //     for (int i = 0; i < minigameCount; i++)
+    //     {
+    //         Debug.Log($"Starting journey minigame {i + 1}/{minigameCount}...");
+    //         // StartMissionLoop(); 
+            
+    //         // TODO: Replace this with an actual wait until the minigame is fully completed by all players.
+
+    //         await UniTask.Delay(5000); //placeholder delay (5s)
+    //     }
+    // }
 }
