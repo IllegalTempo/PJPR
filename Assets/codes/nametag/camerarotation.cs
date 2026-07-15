0c4ef57d76a6b2dbc624699a46c6adaddec1db14
Your Name <you@example.com>
2026-07-15 15:37:06 +0100

    nametag

---
+++ Assets/codes/nametag/camerarotation.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class UIrotator : MonoBehaviour
+{
+    private Transform trans;
+    private Vector3 offset = new Vector3(0, 180, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trans = GameObject.Find("Camera").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(trans);
+        transform.Rotate(offset);
+    }
+}
